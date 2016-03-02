1a661bbfd5d85e8d0366963bc891e19fd76646fb
a1500872 <a1500872@myy.haaga-helia.fi>
2016-03-02 13:33:46 +0200

    PizzaDAO, jossa perustiedot PizzeriaAdminin tiedoista (täytteiden haku puuttuu!).

---
+++ ihasama/src/dao/PizzaDAO.java
@@ -0,0 +1,100 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import luokat.Pizza;
+
+
+
+public class PizzaDAO {
+
+	// Tietokanta-ajurin lataus siirretty constructoriin
+
+	public PizzaDAO() {
+
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (java.lang.ClassNotFoundException ex) {
+			System.out.print("ClassNotFoundException: ");
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	public ArrayList<Pizza> pizzalista = new ArrayList<Pizza>();
+	Connection yhteys = null;
+
+	public void avaaYhteys() {
+		// K‰ytt‰j‰tiedot ja DB:n osoite - lis‰‰ puuttuvat tiedot!
+		String username = "";
+		String password = "";
+		String url = "jdbc:mariadb://localhost/a#######";
+
+		try {
+			// Yhdistet‰‰n tietokantaan
+			yhteys = DriverManager.getConnection(url, username, password);
+		} catch (SQLException ex) {
+			System.out.println("Virhe yhteyden avaamisessa");
+		}
+
+	}
+
+	public void suljeYhteys() {
+		try {
+			if (yhteys != null && !yhteys.isClosed())
+				yhteys.close();
+		} catch (Exception e) {
+			System.out.println("Tietokantayhteys ei jostain syyst√§ suostu menem√§√§n kiinni.");
+			e.printStackTrace();
+		}
+	}
+
+	public List<Pizza> haePizzat() throws NumberFormatException, SQLException {
+		String sql = "select * from pizza";
+		Statement haku = yhteys.createStatement();
+		ResultSet tulokset = haku.executeQuery(sql);
+
+		while (tulokset.next()) {
+			int id = tulokset.getInt("id");
+			String nimi = tulokset.getString("pizzanimi");
+			String hintax = tulokset.getString("hinta");
+			double hinta = Double.parseDouble(hintax.replace(",", "."));
+			// lis‰t‰‰n pizza listaan
+			Pizza pizza = new Pizza(id, nimi, hinta);
+			pizzalista.add(pizza);
+		}
+
+		return pizzalista;
+
+	}
+
+	public void lisaaPizza(Pizza p) {
+		
+
+		try {
+			// alustetaan sql-lause
+			String sql = "insert into pizza(pizzanimi, hinta) values(?,?)";
+			PreparedStatement lause = yhteys.prepareStatement(sql);
+
+			// t‰ydennet‰‰n puuttuvat tiedot (eli pizzan nimi ja hinta)			
+			lause.setString(1, p.getPizzanimi());
+			lause.setDouble(2, p.getHinta());
+
+			// suoritetaan lause
+			lause.executeUpdate();
+
+			System.out.println("Pizza" + p + "lis‰tty tietokantaan");
+		} catch (Exception e) {
+			// Tapahtui jokin virhe
+			System.out.println("Pizzan lis‰‰misyritys aiheutti virheen!");
+		} 
+
+	}
+
+}
