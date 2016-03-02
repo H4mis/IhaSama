2beaac6a3f041e0686db1e37ca7168ba03b086c9
a1500919 <a1500919@pao5015z24.haagahelia.amk>
2016-03-02 12:58:24 +0200

    Insert tablen luominen (pizza täyte) -Seba ja niki

--- ihasama/SQL/Insert into tables.sql
+++ ihasama/SQL/Insert into tables.sql
@@ -1,5 +1,43 @@
 /*Esimerkki*/
 
-INSERT INTO table_name (column1,column2,column3,...)
-VALUES (value1,value2,value3,...);
+INSERT INTO Pizza (nimi,hinta)
+VALUES ('Margareta', 10.00);
 
+INSERT INTO Pizza (nimi,hinta)
+VALUES ('Tropicana', 14.99);
+
+INSERT INTO Pizza (nimi,hinta)
+VALUES ('Porakana', 18.00);
+
+INSERT INTO Pizza (nimi,hinta)
+VALUES ('Alkopala', 19.50);
+
+
+/* ------------------------------------ */
+
+
+INSERT INTO Tayte (nimi, saatavuus)
+VALUES ('Kinkku', 'TRUE');
+
+INSERT INTO Tayte (nimi, saatavuus)
+VALUES ('Juusto', 'TRUE');
+
+INSERT INTO Tayte (nimi, saatavuus)
+VALUES ('Jauheliha', 'TRUE');
+
+INSERT INTO Tayte (nimi, saatavuus)
+VALUES ('Ananas', 'TRUE');
+
+INSERT INTO Tayte (nimi, saatavuus)
+VALUES ('Tomaatti', 'TRUE');
+
+INSERT INTO Tayte (nimi, saatavuus)
+VALUES ('Kebab', 'TRUE');
+
+INSERT INTO Tayte (nimi, saatavuus)
+VALUES ('Salami', 'TRUE');
+
+INSERT INTO Tayte (nimi, saatavuus)
+VALUES ('Kana', 'TRUE');
+
+/* ------------------------------------ */
