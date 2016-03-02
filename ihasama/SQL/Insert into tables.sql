d46307cd1f9ad977c0022bad987e316469bc1a39
a1500919 <a1500919@pao5015z24.haagahelia.amk>
2016-03-02 13:19:35 +0200

    Insert tablen päivitys -seba niki

--- ihasama/SQL/Insert into tables.sql
+++ ihasama/SQL/Insert into tables.sql
@@ -1,43 +1,43 @@
 /*Esimerkki*/
 
-INSERT INTO Pizza (nimi,hinta)
+INSERT INTO Pizza (pizzanimi,hinta)
 VALUES ('Margareta', 10.00);
 
-INSERT INTO Pizza (nimi,hinta)
+INSERT INTO Pizza (pizzanimi,hinta)
 VALUES ('Tropicana', 14.99);
 
-INSERT INTO Pizza (nimi,hinta)
+INSERT INTO Pizza (pizzanimi,hinta)
 VALUES ('Porakana', 18.00);
 
-INSERT INTO Pizza (nimi,hinta)
+INSERT INTO Pizza (pizzanimi,hinta)
 VALUES ('Alkopala', 19.50);
 
 
 /* ------------------------------------ */
 
 
-INSERT INTO Tayte (nimi, saatavuus)
+INSERT INTO Tayte (taytenimi, saatavilla)
 VALUES ('Kinkku', 'TRUE');
 
-INSERT INTO Tayte (nimi, saatavuus)
+INSERT INTO Tayte (taytenimi, saatavilla)
 VALUES ('Juusto', 'TRUE');
 
-INSERT INTO Tayte (nimi, saatavuus)
+INSERT INTO Tayte (taytenimi, saatavilla)
 VALUES ('Jauheliha', 'TRUE');
 
-INSERT INTO Tayte (nimi, saatavuus)
+INSERT INTO Tayte (taytenimi, saatavilla)
 VALUES ('Ananas', 'TRUE');
 
-INSERT INTO Tayte (nimi, saatavuus)
+INSERT INTO Tayte (taytenimi, saatavilla)
 VALUES ('Tomaatti', 'TRUE');
 
-INSERT INTO Tayte (nimi, saatavuus)
+INSERT INTO Tayte (taytenimi, saatavilla)
 VALUES ('Kebab', 'TRUE');
 
-INSERT INTO Tayte (nimi, saatavuus)
+INSERT INTO Tayte (taytenimi, saatavilla)
 VALUES ('Salami', 'TRUE');
 
-INSERT INTO Tayte (nimi, saatavuus)
+INSERT INTO Tayte (taytenimi, saatavilla)
 VALUES ('Kana', 'TRUE');
 
 /* ------------------------------------ */
