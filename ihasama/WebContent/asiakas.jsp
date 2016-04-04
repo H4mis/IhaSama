0cceb15884c6c247408a717cf1c8c346d7907d9b
a1500957 <a1500957@pao5015z08.haagahelia.amk>
2016-04-04 14:13:48 +0300

    Asiakkaalle lisätty oma kontrolleri, pizzalistan tulostus asiakkaan sivulle wörkkii! -Jenni

--- ihasama/WebContent/asiakas.jsp
+++ ihasama/WebContent/asiakas.jsp
@@ -1,4 +1,3 @@
-
 <%@page import="java.text.DecimalFormat"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 
@@ -14,7 +13,7 @@
 <script src="jquery-1.12.0.min.js"></script>
 <link rel="stylesheet" type="text/css" href="tyylit/tyyli.css">
 <link href='https://fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'>
-<title>Asiakassivu</title>
+<title>Menu</title>
 
 </head>
 <body>
@@ -22,8 +21,23 @@
 
 
 <h1>Castello é Fiore</h1>
-
-
+ 
+<table>
+					<tr>
+						<td><p>Pizzat</p></td>
+						<td><p>Hinta</p></td>
+						
+					</tr>
+				<c:forEach items="${pizzalista}" var="pizza">
+					<tr>
+						<td><c:out value="${pizza.pizzanimi}"/><br>
+							<div class="taytenimi"><c:out value="${pizza.taytteet}"/></div>
+						</td>
+						<td><c:out value="${pizza.hinta}"/></td>
+						
+					</tr>
+				</c:forEach>
+			</table>
 
 </body>
 </html>
---
+++ ihasama/src/servlet/asiakasKontrolleri.java
@@ -0,0 +1,122 @@
+
+package servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import luokat.Pizza;
+import luokat.Tayte;
+import dao.PizzaDAO;
+
+/**
+ * Servlet implementation class asiakasKontrolleri
+ */
+@WebServlet("/asiakasKontrolleri")
+public class asiakasKontrolleri extends HttpServlet {
+	
+	//lis‰t‰‰n decimal formatti ett‰ n‰kyy nolla hinnan per‰ss‰
+		DecimalFormat f = new DecimalFormat("0.00");
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public asiakasKontrolleri() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// Luodaan oliolista tietokannan tiedoista
+
+		PizzaDAO pDAO = new PizzaDAO();
+		pDAO.avaaYhteys();
+		List<Pizza> lista;
+		try {
+			lista = pDAO.haePizzat();
+			request.setAttribute("pizzalista", lista);
+			
+		} catch (NumberFormatException e) {
+
+			e.printStackTrace();
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		} finally {
+			pDAO.suljeYhteys();
+		}
+
+		// Pistet‰‰n tieto eteenp‰in asiakas.jsp:lle
+		
+		RequestDispatcher disp = request.getRequestDispatcher("asiakas.jsp");
+		disp.forward(request, response);
+		
+
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		//luodaan pizzalista
+				List<Pizza> lista = new ArrayList<Pizza>();		
+				String taytteet = "hahaa";
+				//haetaan parametrit k‰ytt‰j‰lt‰(submitista) ja lis‰t‰‰n nelopuksi pizza-olioon
+				String nimi = request.getParameter("nimi");
+				String tnimi = request.getParameter("taytenimi");
+				if(nimi != null && !nimi.isEmpty()){
+				double hinta = Double.parseDouble(request.getParameter("hinta"));
+				int id = lista.size() +1;
+				String[] taytteidenIdt = request.getParameterValues("taytteet");
+//				taytteet = taytteidenIdt.length;
+				
+				
+				Pizza a = new Pizza(id, nimi, hinta, taytteet);
+				
+				//lis‰t‰‰n pizza-olio tietokantaan PizzaDAO-java luokan avulla.
+				PizzaDAO pDao = new PizzaDAO();
+				pDao.avaaYhteys();
+				pDao.lisaaPizza(a);
+				pDao.lisaaPizzantaytteet(a, taytteidenIdt);
+				pDao.suljeYhteys();
+				
+				response.setContentType("text/html");
+			    //java.io.PrintWriter wout = response.getWriter();
+			    System.out.println("T‰ytteet:" +a.getTaytteet());  
+			    System.out.println("<b>Nimi:</b> " + a.getPizzanimi());
+			    System.out.println("<br>");
+			    System.out.println("<b>Hinta:</b> " + f.format(a.getHinta()));
+				  
+			    //ohjataan takaisin alkuun
+			  	response.sendRedirect("asiakasKontrolleri?added=true");
+				}
+				if(tnimi != null && !tnimi.isEmpty()){
+					Tayte t = new Tayte(1, tnimi, true);
+					PizzaDAO pDao = new PizzaDAO();
+					pDao.avaaYhteys();
+					pDao.lisaaTayte(t);			
+					pDao.suljeYhteys();
+					response.sendRedirect("asiakasKontrolleri?added=true");
+				}
+				
+				}
+	}
+
+
