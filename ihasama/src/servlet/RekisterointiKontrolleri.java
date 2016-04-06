37b167fc39dbf95ba67e0b631431dd4bc839ea37
a1500957 <a1500957@pao5015z08.haagahelia.amk>
2016-04-06 14:46:59 +0300

    pieni muutos rekisteröinti kontrolleriin -Jenni

--- ihasama/src/servlet/RekisterointiKontrolleri.java
+++ ihasama/src/servlet/RekisterointiKontrolleri.java
@@ -12,6 +12,11 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class UserDataServlet
  */
 public class RekisterointiKontrolleri extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("text/html");
